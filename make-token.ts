45aced8ad93627a289d1e57b718239b7fd07360e
katpavan <45500874+katpavan@users.noreply.github.com>
2024-03-25 15:53:25 -0400

    make token

---
+++ make-token.ts
@@ -0,0 +1,28 @@
+import * as token from "@solana/spl-token";
+import "dotenv/config";
+import { 
+    getKeypairFromEnvironment,
+    getExplorerLink,
+} from "@solana-developers/helpers";
+
+import { Connection, clusterApiUrl } from "@solana/web3.js";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(`Loaded our keypair securely, using an env file! Our public key is: ${keypair.publicKey.toBase58()}`);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const sender = getKeypairFromEnvironment("SECRET_KEY");
+
+const tokenMint = await token.createMint(
+    connection,
+    sender,
+    sender.publicKey,
+    null,
+    2
+)
+
+const link = getExplorerLink("address", tokenMint.toString(), "devnet");
+
+console.log(`Token mint address: ${link}`);
