62860b1ccf5f713682394584629c81ed5ebdd976
katpavan <45500874+katpavan@users.noreply.github.com>
2024-03-26 10:44:44 -0400

    start: make a token account for a recipient

---
+++ make-token-account.ts
@@ -0,0 +1,12 @@
+import { getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
+import "dotenv/config";
+import {
+    getExplorerLink,
+    getKeypairFromEnvironment,
+} from "@solana-developers/helpers";
+import {Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const sender = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(`Loaded our keypair securely, using an env file! Our public key is: ${sender.publicKey.toBase58()}`);
